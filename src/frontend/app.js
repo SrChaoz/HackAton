a31e8a01d9558793f5c9b960ab4baea452917609
Marlon Mendoza <marloveper__@MacBook-Air-de-Marlon.local>
2025-01-23 14:41:18 -0500

    arreglo barra de busqueda 14:41 - 23/01/2025

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -18,6 +18,13 @@ function fetchSearchEngine() {
         .catch(error => console.error("Error:", error));
 }
 
+let debounceTimer;
+function debounceFetchSearch() {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(fetchSearchEngine, 300); // 300ms de espera
+}
+
+
 async function fetchRankingData() {
     try {
         // Realiza la solicitud al backend
